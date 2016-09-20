587f147e21dd45142ad709684011df7ac62fb19c
DavidSunter <davidsunter@skybettingandgaming.com>
2016-09-20 15:31:56 +0100

    delete key

--- deploy.php
+++ deploy.php
@@ -1,9 +1,5 @@
 <?php
   echo "Deploying Code";
-  $key = $_POST['payload'];
-  if ($key == "password") {
-    $message=shell_exec("/root/www/deploy.sh");
-    print_r($message);
-  }
-
+  $message=shell_exec("/root/www/deploy.sh");
+  print_r($message);
  ?>
