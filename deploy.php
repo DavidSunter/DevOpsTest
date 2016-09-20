9b15f09b6467a26ae6f4772d0e159102d4fb87ff
DavidSunter <davidsunter@skybettingandgaming.com>
2016-09-20 15:19:30 +0100

    added key

--- deploy.php
+++ deploy.php
@@ -1,5 +1,9 @@
 <?php
   echo "Deploying Code";
-  $message=shell_exec("/root/www/deploy.sh");
-  print_r($message);
+  $key = $_POST['payload'];
+  if ($key == "password") {
+    $message=shell_exec("/root/www/deploy.sh");
+    print_r($message);
+  }
+
  ?>
